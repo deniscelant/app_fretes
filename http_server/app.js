2da58355bc8c8336819d42477673ae79d0dde927
Denis Celant <dedecelant123@gmail.com>
2025-05-07 17:30:06 -0300

    form  validation

--- http_server/app.js
+++ http_server/app.js
@@ -1,4 +1,5 @@
 const urlLogin = "http://localhost:8080/http_server/login.html";
+const urlDB = "http://localhost:8080/http_server/db.json";
 
 // fetch(urlLogin)
 //   .then((response) => response.text()) // ou .json() se for JSON
@@ -10,5 +11,19 @@ const urlLogin = "http://localhost:8080/http_server/login.html";
 //     console.error("Erro ao buscar dados:", error);
 //   });
 
-fetch(urlLogin)
-  .then((response) => console.log(response)) // ou .json() se for JSON
+fetch(urlDB)
+  .then((req) => req.json())
+  .then((res) => {
+    console.log(res.users)
+    document.querySelector("#loginForm").addEventListener("submit", () => {
+      if (
+        document.querySelector("#name").value === res.users.nome &&
+        document.querySelector("#tel").value === res.users.tel &&
+        document.querySelector("#cep").value === res.users.cep
+      ) {
+        alert("logado.");
+      } else {
+        alert("informações incorretas.");
+      }
+    });
+  });
