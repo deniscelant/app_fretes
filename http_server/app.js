3d15b27b8d724f339c55ce6718121fced00e9e37
Denis Celant <dedecelant123@gmail.com>
2025-05-08 13:45:20 -0300

    get address

--- http_server/app.js
+++ http_server/app.js
@@ -2,30 +2,33 @@ const urlLogin = "http://localhost:8080/http_server/login.html";
 const urlDB = "http://localhost:8080/http_server/db.json";
 const urlHub = "http://localhost:8080/http_server/hub.html";
 
-// fetch(urlLogin)
-//   .then((response) => response.text()) // ou .json() se for JSON
-//   .then((text) => {
-//     const welcome = document.getElementById("welcome");
-//     welcome.innerHTML = "Seja bem-vindo " + text;
-//   })
-//   .catch((error) => {
-//     console.error("Erro ao buscar dados:", error);
-//   });
-
-fetch(urlDB)
-  .then((req) => req.json())
-  .then((res) => {
-    console.log(res.users);
-    document.querySelector("#loginForm").addEventListener("submit", () => {
-      if (
-        document.querySelector("#name").value == res.users.nome &&
-        document.querySelector("#tel").value == res.users.tel &&
-        document.querySelector("#cep").value == res.users.cep
-      ) {
-        alert("logado.");
-      } else {
-        alert("informações incorretas.");
-      }
+if (document.title === "Hub") {
+  fetch(urlDB)
+    .then((response) => response.json()) // ou .json() se for JSON
+    .then((text) => {
+      const welcome = document.getElementById("welcome");
+      welcome.innerHTML = "Seja bem-vindo " + text.users.nome;
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar dados:", error);
     });
-  });
+}
 
+if (document.title === "Login") {
+  fetch(urlDB)
+    .then((req) => req.json())
+    .then((res) => {
+      console.log(res.users);
+      document.querySelector("#loginForm").addEventListener("submit", () => {
+        if (
+          document.querySelector("#name").value == res.users.nome &&
+          document.querySelector("#tel").value == res.users.tel &&
+          document.querySelector("#cep").value == res.users.cep
+        ) {
+          alert("logado.");
+        } else {
+          alert("informações incorretas.");
+        }
+      });
+    });
+}
