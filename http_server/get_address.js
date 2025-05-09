5776c92413027bcf8d712fa5e34b3d4ec33e2f05
Denis Celant <dedecelant123@gmail.com>
2025-05-09 13:41:14 -0300

    class pedido

--- http_server/get_address.js
+++ http_server/get_address.js
@@ -1,3 +1,10 @@
-function nextCompanies(){
-    
+const cep = "85812003";
+const region = cep[0];
+const subregion = cep[1];
+const sector = cep[2];
+
+class Pedido{
+    constructor(info, region){
+        
+    }
 }
